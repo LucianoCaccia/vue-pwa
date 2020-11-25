47643a5d6779960917676829c8df9634484fbfcd
LC <luciano.caccia@tooit.com>
2020-11-25 15:12:27 -0300

    adding test radios to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,16 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <the-jumbotron v-bind:msg="msg"></the-jumbotron>
+    <div class="container">
+      <h4>Metro 95.1</h4>
+      <audio controls="controls">Metro<source src="http://mp3.metroaudio1.stream.avstreaming.net:7200/metro" type="audio/mpeg" /></audio>
+    </div>
+    <div class="container mt-4 mb-5">
+      <h4>Aspen 102.3</h4>
+      <audio controls="controls">Aspen<source src="https://15363.live.streamtheworld.com/ASPENAAC_SC" type="audio/mpeg" /></audio>
+    </div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
